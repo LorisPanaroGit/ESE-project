8f4b40f60e4118d4f6499063e0a8fdb889bfb3b0
LorisPanaroGit <lorispanaro28@gmail.com>
2023-01-13 22:09:06 +0100

    Class 5C PM2.5 sensor

---
+++ Schematic_FinalProject/allegro/zip_art.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -r gerbers.zip
+zip gerbers.zip AIRQUALITYBLUETOOTHSENSOR-1-2.drl BOTTOM.art OUTLINE.art PM_BOT.art PM_TOP.art SILK_BOT.art SILK_TOP.art SM_BOT.art SM_TOP.art TOP.art                                                      
+
+
+
+
